f11b3e5de55e16957b70f0f3dff492b077665ba6
jcreech72 <89232631+jcreech72@users.noreply.github.com>
2021-08-24 20:16:19 -0500

    Add files via upload

---
+++ bonus.py
@@ -0,0 +1,6 @@
+#44608-80 Mod 1 bonus
+"""10 data points min max and range"""
+print('MIN',min(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+print('MAX',max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+print('Range:', min(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), '-', max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+"""End of File"""
